9749f58b666d337843cfe794c688b75c89efcfce
diwayou <diwayou19850811>
2016-11-14 17:34:45 +0800

    修改hessian序列化不检查Serializable，这样使用的时候就不需要各种实现Serializable了

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/serialize/support/hessian/Hessian2SerializerFactory.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/serialize/support/hessian/Hessian2SerializerFactory.java
@@ -19,14 +19,15 @@ import com.alibaba.com.caucho.hessian.io.SerializerFactory;
 
 public class Hessian2SerializerFactory extends SerializerFactory {
 
-	public static final SerializerFactory SERIALIZER_FACTORY = new Hessian2SerializerFactory();
+    public static final SerializerFactory SERIALIZER_FACTORY = new Hessian2SerializerFactory();
 
-	private Hessian2SerializerFactory() {
-	}
+    private Hessian2SerializerFactory() {
+		setAllowNonSerializable(true);
+    }
 
-	@Override
-	public ClassLoader getClassLoader() {
-		return Thread.currentThread().getContextClassLoader();
-	}
+    @Override
+    public ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
 
 }
